b9d4d02e31a5ea9e916b2d618ab043996e2cd88d
Moïse <moise.rajesearison@mvola.mg>
2025-09-09 21:54:21 +0300

    add timer using BLoC

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
-import 'bloc_state_management/flutter_counter/counter_app.dart';
-import 'bloc_state_management/flutter_counter/counter_observer.dart';
+import 'bloc_state_management/flutter_timer/timer_app.dart';
 
-void main() {
-  Bloc.observer = const CounterObserver();
-  runApp(CounterApp());
-}
+void main() => runApp(const App());
+
+
+
+
+
+
+
+// import 'package:bloc/bloc.dart';
+// import 'package:flutter/material.dart';
+
+// import 'bloc_state_management/flutter_counter/counter_app.dart';
+// import 'bloc_state_management/flutter_counter/counter_observer.dart';
+
+// void main() {
+//   Bloc.observer = const CounterObserver();
+//   runApp(CounterApp());
+// }
+
 
